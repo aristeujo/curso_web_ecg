e9b43f95ffb30aaefad81d7bf4bdee1928c25946
aristeujo <jasc.eai18@uea.edu.br>
2025-10-02 13:33:14 -0400

    feat: add patients table

--- backend/app/db_models.py
+++ backend/app/db_models.py
@@ -22,4 +22,18 @@ class User(Base):
     phone: Mapped[str] = mapped_column(String(20), nullable=False, index=True, unique=True)
     password_hash: Mapped[str] = mapped_column(String(255), nullable=False)
 
+class Patient(Base):
+    __tablename__ = "patients"
 
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    name: Mapped[str] = mapped_column(String(50), nullable=False, index=True) 
+    birth_date: Mapped[Date | None] = mapped_column(Date, nullable=False, index=True)
+    email: Mapped[str] = mapped_column(String(255), nullable=False, index=True, unique=True)
+    phone: Mapped[str] = mapped_column(String(20), nullable=False, index=True, unique=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    
+    # triages: Mapped[list["Triage"]] = relationship(
+    #     back_populates="patient", 
+    #     cascade="all, delete-orphan",
+    #     passive_deletes=True,
+    # )
